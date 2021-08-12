6a81331330446799ecd099c25a94419500549c21
Maksim <mgubkov@gmail>
2021-08-12 16:09:32 +0700

    task3

---
+++ src/com/company/task3/Apple.java
@@ -0,0 +1,9 @@
+package com.company.task3;
+
+public class Apple extends Fruit{
+
+    @Override
+    public double getWeight() {
+        return 1.0;
+    }
+}
---
+++ src/com/company/task3/Fruit.java
@@ -0,0 +1,5 @@
+package com.company.task3;
+
+public abstract class Fruit {
+    public abstract double  getWeight();
+}
